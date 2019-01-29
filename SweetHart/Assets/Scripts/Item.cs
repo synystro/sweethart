af880077215a004e1bb3128544a4fa25e4c10606
Lux Game Studio <gamestudiolux@gmail.com>
2019-01-29 19:44:49 -0200

    item manager implemented with basic randomized spawning system (needs improvements e.g. not to spawn a key inside its locker)

---
+++ SweetHart/Assets/Scripts/Item.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Item : ItemManager
+{
+    [SerializeField] private string itemName;
+    
+    public string ItemName { get { return itemName; } set { itemName = value; } }
+
+    private void Start()
+    {
+
+    }
+    private void Update()
+    {
+
+    }
+}
---
+++ SweetHart/Assets/Scripts/ItemManager.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class ItemManager : MonoBehaviour
+{
+    [Header("Items")]
+    [SerializeField] private string[] itemNames;
+
+    [Header("Spawned?")]
+    [SerializeField] private bool backDoorKeySpawned;
+
+    private GameObject[] itemBoxes;
+    private GameObject randomizedItemBox;
+    private int randomItemBox;
+
+    private void Start()
+    {
+        // shuffle items.
+        ShuffleArray(itemNames);
+        // find item boxes.
+        itemBoxes = GameObject.FindGameObjectsWithTag("ItemBox");
+
+        for (int i = 0; i < itemBoxes.Length; i++)
+        {
+            Item itemBox = itemBoxes[i].GetComponent<Item>();
+            itemBox.ItemName = itemNames[i];
+            itemBoxes[i].tag = "Item";
+        }
+    }
+
+    private void ShuffleArray(string[] texts)
+    {
+        for(int i = 0; i < texts.Length; i++)
+        {
+            string tmp = texts[i];
+            int r = UnityEngine.Random.Range(i, texts.Length);
+            texts[i] = texts[r];
+            texts[r] = tmp;
+        }
+    }
+}
